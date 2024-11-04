7eff43be45f1ba21f0f17330014faf8beee2101f
hai hong <133628015+thh1809@users.noreply.github.com>
2024-11-04 14:38:52 +0700

    profile

--- source/Team13-FE/src/components/Routes/index.jsx
+++ source/Team13-FE/src/components/Routes/index.jsx
@@ -1,11 +1,16 @@
 import Homepage from "../pages/Homepage/Homepage"
 import ChangePassword from "../pages/ChangePasswordPage/ChangePassword.jsx"
 import SignupForm from "../pages/SignupPage/SignupForm.jsx"
+import ProfileSettings from "../pages/Profile/ProfileSetting.jsx"
+import EditProfile from"../pages/Profile/EditProfile.jsx"
+
 
 const PublicRoutes = [
     {path:"/*",component:Homepage,layout:null},
     {path:"/changepassword",component:ChangePassword,layout:null},
     {path:"/signup",component:SignupForm,layout:null},
+    {path:"/profilesetting",component:ProfileSettings,layout:null},
+    {path:"/editprofile",component:EditProfile,layout:null},
 
 ]
 
